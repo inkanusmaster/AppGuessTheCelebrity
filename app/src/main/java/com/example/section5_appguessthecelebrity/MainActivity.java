6aa2bfe4bb4cbf273f13c4664d07c12bea39acaf
inkanusmaster <furymistrz@gmail.com>
2019-05-29 22:19:30 +0200

    Losuje klucze - imiona i nazwiska do buttonów Jak wylosować 1 wartość z tych 4 kluczy (obrazek)?

--- app/src/main/java/com/example/section5_appguessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/section5_appguessthecelebrity/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,19 +15,27 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    String result;
+    String result, keyCelebrityName;
     Pattern p;
     Matcher m;
     Map<String, String> celebrityMap = new HashMap<>();
+    GridLayout buttonGridLayout;
+    Button button;
+
+    public void initializeVars(){
+        buttonGridLayout = findViewById(R.id.buttonGridLayout);
+    }
 
 
     @SuppressLint("StaticFieldLeak")
-    public class DownloadTask extends AsyncTask<String, Void, String>{
+    public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... httpUrl) {
@@ -41,13 +51,13 @@ public class MainActivity extends AppCompatActivity {
                 InputStreamReader reader = new InputStreamReader(in);
                 int data = reader.read();
 
-                while (data !=-1){
+                while (data != -1) {
                     char current = (char) data;
                     result.append(current);
                     data = reader.read();
                 }
                 return result.toString();
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
                 return "Failed!";
             }
@@ -56,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void downloadContent(){
+    public void downloadContent() {
 
         result = null;
         celebrityMap.clear();
@@ -64,22 +74,22 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             result = task.execute("http://www.posh24.se/kandisar").get();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         try {
             p = Pattern.compile("<img src=\"(.*?)\" alt=\"(.*?)\"/>");
             m = p.matcher(result);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         try {
-            while (m.find()){
-                celebrityMap.put(m.group(2),m.group(1));
+            while (m.find()) {
+                celebrityMap.put(m.group(2), m.group(1));
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println(celebrityMap);
@@ -87,8 +97,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void checkAnswer(View view) {
 
-    public void checkAnswer(View view){
+    }
+
+    @SuppressLint("NewApi")
+    public void randomCelebrities() {
+
+        for (int i = 0; i < buttonGridLayout.getChildCount(); i++) {
+            keyCelebrityName = (String) Objects.requireNonNull(celebrityMap.keySet().toArray())[new Random().nextInt(Objects.requireNonNull(celebrityMap.keySet().toArray()).length)];
+            button = (Button) buttonGridLayout.getChildAt(i);
+            button.setText(keyCelebrityName);
+        }
+
 
     }
 
@@ -96,7 +117,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeVars();
         downloadContent();
+        randomCelebrities();
     }
 
 }
